ef68a6e509175b40a5b88558115715558609c61b
[gitavellas] <[situmeang.gita@gmail.com]>
2020-03-07 10:36:15 +0700

    add my_history

--- app/src/main/java/com/example/tubes/activity/MyHistoryActivity.java
+++ app/src/main/java/com/example/tubes/activity/MyHistoryActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.tubes.Model.MyHistory;
 import com.example.tubes.R;
@@ -16,6 +19,7 @@ public class MyHistoryActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MyHistoryAdapter adapter;
     private ArrayList<MyHistory> historyArrayList;
+    private ImageView mBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,44 +30,48 @@ public class MyHistoryActivity extends AppCompatActivity {
     }
 
     private void initUI() {
+        //init
+        mBack = findViewById(R.id.back_button);
+        //add data
         addDataHistory();
 
+        //recycle view
         recyclerView = (RecyclerView) findViewById(R.id.history_recycle);
         adapter = new MyHistoryAdapter(historyArrayList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MyHistoryActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
 
+        //listener button
+        backListener();
+
+    }
+
+    private void backListener() {
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     private void addDataHistory() {
         historyArrayList = new ArrayList<>();
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
-        historyArrayList.add(new MyHistory("03-Agustus-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
+        historyArrayList.add(new MyHistory("03-Agust-2020", "Persembahan", 100000));
     }
 }
